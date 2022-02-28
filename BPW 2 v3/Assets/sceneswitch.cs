63c7c08d1d2dc0b6b6017df9056abef5695eb33a
Ronan Stam <Ronan.stam@gmail.com>
2022-02-28 20:36:18 +0100

    aa

---
+++ BPW 2 v3/Assets/sceneswitch.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sceneswitch : MonoBehaviour
+{
+    public GameObject blockAppear;
+    // Start is called before the first frame update
+    void Start()
+    {
+        blockAppear.SetActive = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerStay (Collider other)
+    {
+        blockAppear.SetActive = true;
+    }
+}
