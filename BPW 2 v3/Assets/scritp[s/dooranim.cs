735181e51ea41cf773d9f7cd11488361ccfdd6e7
Ronan Stam <Ronan.stam@gmail.com>
2022-03-02 16:37:47 +0100

    working door

---
+++ BPW 2 v3/Assets/scritp[s/dooranim.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dooranim : MonoBehaviour
+{
+    public Animator Door;
+    bool ePress = false;
+    public bool Collided;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Door.GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("e"))
+        {
+            ePress = true;
+        } else
+        {
+            ePress = false;
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (ePress == true && !Collided)
+        {
+            Collided = true;
+            Debug.Log("fuck");
+            Door.SetTrigger("activate");
+        }
+    }
+}
